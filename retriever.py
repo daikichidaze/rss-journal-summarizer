2ea47d34f1576b6c40a7fe33f76604e9a707d878
daikichidaze <dkch.yoshioka.t@gmail.com>
2024-04-14 14:43:58 +0700

    changed to Abstract Base Class

--- retriever.py
+++ retriever.py
@@ -1,41 +1,50 @@
+from abc import ABC, abstractmethod
 from datetime import datetime, timedelta
 from time import mktime
-
 import feedparser
 import requests
-import pytz
 from typing import List
 from bs4 import BeautifulSoup
 
 from run_date_mgt import get_current_datetime, ICT
 
 
-class InternationalOrganizationRetriever:
-    name = 'International Organization'
-    rss_url = 'https://www.cambridge.org/core/rss/product/id/146C8B1E6606CE283EBC5B10B255F4C0'
+class Retriever(ABC):
+    name: str
+    rss_url: str
 
     def fetch_recent_entries(self, current_datetime=None, start_datetime=None):
         feed = feedparser.parse(self.rss_url)
         entries = feed.entries
-        recent_entries = retrieve_recent_entries(
-            entries, current_datetime=current_datetime, start_datetime=start_datetime)
-        return recent_entries
+        return retrieve_recent_entries(entries, current_datetime=current_datetime, start_datetime=start_datetime)
 
+    @abstractmethod
     def extract_abstract(self, entry) -> str:
-        # Get page contents
+        """Common page retrieval and parsing process"""
         response = requests.get(entry.link)
-
-        # Parse page contents
         soup = BeautifulSoup(response.text, 'html.parser')
+        return self.parse_abstract(soup)
 
+    @abstractmethod
+    def parse_abstract(self, soup) -> str:
+        """Abstract method to define the specific abstract parsing method in subclasses"""
+        pass
+
+    @abstractmethod
+    def get_entry_property(self, entry) -> dict:
+        pass
+
+
+class InternationalOrganizationRetriever(Retriever):
+    name = 'International Organization'
+    rss_url = 'https://www.cambridge.org/core/rss/product/id/146C8B1E6606CE283EBC5B10B255F4C0'
+
+    def parse_abstract(self, soup) -> str:
         abstract_class = soup.find(class_='abstract')
-
-        if abstract_class is None:
+        if abstract_class is None:  # In case not a article page
             return ''
-
         abstract_contents = abstract_class.find_all('p', recursive=False)[0]
         abstract_text = abstract_contents.contents[0]
-
         return abstract_text
 
     def get_property(self, entry) -> dict:
@@ -44,7 +53,6 @@ class InternationalOrganizationRetriever:
         url = entry['link']
         title = entry['title']
         authors = ';'.join([item.name for item in entry['authors']])
-
         return {
             'journal': journal,
             'date': date,
@@ -59,14 +67,8 @@ def check_entry_recent(entry, current_datetime=None, start_datetime=None):
         current_datetime = get_current_datetime()
     if start_datetime is None:
         start_datetime = current_datetime - timedelta(days=1)
-
-    entry_time = datetime.fromtimestamp(
-        mktime(entry.updated_parsed), tz=ICT)
-
-    if (start_datetime <= entry_time < current_datetime):
-        return True
-
-    return False
+    entry_time = datetime.fromtimestamp(mktime(entry.updated_parsed), tz=ICT)
+    return start_datetime <= entry_time < current_datetime
 
 
 def retrieve_recent_entries(entries, current_datetime=None, start_datetime=None) -> List:
@@ -74,5 +76,4 @@ def retrieve_recent_entries(entries, current_datetime=None, start_datetime=None)
         current_datetime = get_current_datetime()
     if start_datetime is None:
         start_datetime = current_datetime - timedelta(days=1)
-
     return [entry for entry in entries if check_entry_recent(entry, current_datetime, start_datetime)]
