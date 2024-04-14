3fcad1f6023c5c5dbd3b08d8891283a22f20adbd
daikichidaze <dkch.yoshioka.t@gmail.com>
2024-04-14 15:05:13 +0700

    add null date case

--- run_date_mgt.py
+++ run_date_mgt.py
@@ -1,30 +1,43 @@
 import json
-from datetime import datetime, timedelta
+from datetime import datetime
 import os
-
 import pytz
 
-# Indochina Timezone
+# Set timezone to Indochina Time (ICT)
 ICT = pytz.timezone('Asia/Bangkok')
 
 
 def get_current_datetime() -> datetime:
+    """Get the current datetime with ICT timezone."""
     return datetime.now(tz=ICT)
 
 
 def read_last_run_date(file_path):
+    """Read the last run date from a JSON file.
+
+    If the file exists but does not contain the 'last_run_date' key,
+    or if the file does not exist, return a default date.
+    """
+    # Default minimum date with timezone
+    default_date = datetime(1900, 1, 1).replace(tzinfo=ICT)
     if os.path.exists(file_path):
         with open(file_path, 'r') as file:
             data = json.load(file)
-            last_run_date = datetime.strptime(
-                data['last_run_date'], '%Y-%m-%d').replace(tzinfo=ICT)
+            # Check if 'last_run_date' key is in the data
+            if 'last_run_date' in data:
+                last_run_date = datetime.strptime(
+                    data['last_run_date'], '%Y-%m-%d').replace(tzinfo=ICT)
+            else:
+                # Return the default date if the key is not found
+                last_run_date = default_date
     else:
-        last_run_date = datetime(1900, 1, 1).replace(tzinfo=ICT)
+        # Return the default date if the file does not exist
+        last_run_date = default_date
     return last_run_date
 
 
 def update_last_run_date(file_path, new_date):
+    """Update the last run date in a JSON file."""
     with open(file_path, 'w') as file:
         data = {'last_run_date': new_date.strftime('%Y-%m-%d')}
         json.dump(data, file, indent=4)
-    return
